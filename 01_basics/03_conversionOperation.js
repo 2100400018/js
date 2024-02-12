8d47251e8d4a2c0e0b758b1dfc4d955510b05780
2100400018 <112416095+2100400018@users.noreply.github.com>
2024-02-12 16:32:18 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,71 @@
+let score = "33ac"
+let num = null
+
+// console.log(typeof score);
+// console.log(typeof (score));
+// console.log(typeof num);
+
+let valueInNumber = Number(score)
+let newNum = Number(num)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// console.log(typeof newNum);
+// console.log(newNum);
+
+// "33" =>33
+// "33abc" =>NaN
+// true = 1; false = 0
+
+let isLoggedIn = 1 //if it's empty the it will print false
+// and if we print some name then it prints true
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn) //true
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+let someNumber = 33
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue); //-3
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);  // 2^3
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " anuj"
+
+str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(2+1+"1"+2);
+
+/ console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
